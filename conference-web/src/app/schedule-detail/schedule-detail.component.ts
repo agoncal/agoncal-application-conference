2a6177b1cffecfcce967d0583ac11ab52cec06d9
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-02-19 17:53:13 +0100

    Adding schedule detail

--- conference-web/src/app/schedule-detail/schedule-detail.component.ts
+++ conference-web/src/app/schedule-detail/schedule-detail.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {Speaker} from "../server/model/speaker";
+import {Talk} from "../server/model/talk";
+import {Session} from "../server/model/session";
+import {ScheduleApi} from "../server/api/schedule.api";
+import {TalkApi} from "../server/api/talk.api";
+import {SpeakerApi} from "../server/api/speaker.api";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'conf-schedule-detail',
@@ -7,9 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleDetailComponent implements OnInit {
 
-  constructor() { }
+  public session: Session;
+  public talk: Talk;
+  public speakers: Speaker[] = [];
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+              private scheduleService: ScheduleApi,
+              private talkService: TalkApi,
+              private speakerService: SpeakerApi) {
   }
 
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.getSession(id);
+    });
+  }
+
+
+  private getSession(id: string) {
+    this.scheduleService
+      .retrieve(id)
+      .flatMap((s: Session) => {
+        this.session = s;
+        return s ? this.talkService.retrieve(s.talk.id) : undefined;
+      }).map((t: Talk) => {
+      this.talk = t;
+      return t ? t.speakers : undefined;
+    })
+      .subscribe((speakers: Speaker[]) => {
+        speakers.forEach((s) => {
+          this.speakerService.retrieve(s.id).subscribe((sp) => this.speakers.push(sp));
+        });
+      });
+  }
 }
